d3003b6b483292dc598f3f19b1e077d51de21213
NathanB09 <nathanbell09@gmail.com>
2019-01-23 13:38:34 +0000

    added cors and route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:create]
-  resources :relevances, only: [:create]
-  resources :tags, only: [:index, :create]
-  resources :pichas, only: [:index, :show, :create]
+  resources :tags, only: [:index]
+  resources :pichas, only: [:index, :show]
+
+  post 'comments', to: 'comments#create'
+  post 'relevances', to: 'relevances#create'
+  post 'tags', to: 'tags#create'
+  post 'pichas', to: 'pichas#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
